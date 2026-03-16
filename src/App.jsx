f120edcd74548b05b3bf3147af48d57f3887beb9
daryna-budz <darynabudz@gmail.com>
2026-03-15 22:53:36 -0400

    Routes implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
 import { MainScreen } from './components/MainScreen'
 import { QuizScreen } from './components/QuizScreen'
+
 
 function App() {
 
   return (
     <>
-      {/* <MainScreen /> */}
-      <QuizScreen />
+      <Routes>
+         <Route path="/" element={<MainScreen />} />
+         <Route path="/quiz" element={<QuizScreen />} />
+      </Routes>
     </>
   )
 }
--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,11 +1,17 @@
 
+import { Link } from "react-router-dom";
+
 
 export function MainScreen(){
+
+
     return (
         <section className="main-screen">
              <h1>Quizzical</h1>
              <p className="main-description">Check your knowledge!</p>
-             <button className="start-btn">Start quiz</button>
+             <Link to="/quiz">
+                 <button className="start-btn">Start quiz</button>
+             </Link>
 
              <img src="../src/assets/blob-ylw.svg" className="blob blob-yellow" />
              <img src="../src/assets/blob-blue.svg" className="blob blob-blue" />
